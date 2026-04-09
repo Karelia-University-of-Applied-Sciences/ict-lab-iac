5ad96ff76f3fc5e5738ef1965153a53ee9a552a8
alex <118021985+alxcd@users.noreply.github.com>
2026-04-09 17:41:01 +0300

    added example to task to use for new tasks

---
+++ implementation/task_example/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = "2.3.4"
+    }
+  }
+}
+
+# insecure = true: Prism Central uses a self-signed certificate in this lab environment;
+# TLS verification is intentionally disabled as a known deviation from policy.
+provider "nutanix" {
+  endpoint     = var.nutanix_endpoint
+  insecure     = true
+  password     = var.nutanix_password
+  port         = var.nutanix_port
+  session_auth = true
+  username     = var.nutanix_username
+  wait_timeout = 10
+}
+
+# module "project" {
+#   source = "../terraform_modules/nutanix_project"
+#
+#   cluster_uuid        = var.cluster_uuid
+#   default_subnet_uuid = var.default_subnet_uuid
+#   project_name        = var.project_name
+# }
+
+# module "vms" {
+#   depends_on = [module.project]
+#   for_each   = { for user in var.users : user.name => user }
+#
+#   source = "../terraform_modules/nutanix_vm"
+#
+#   cluster_id           = local.cluster_id
+#   disk_size_gb         = var.disk_size_gb
+#   image_uuid           = var.image_uuid
+#   memory_size_gb       = var.memory_size_gb
+#   ngt_iso_uuid         = var.ngt_iso_uuid
+#   num_cores_per_socket = var.num_cores_per_socket
+#   num_sockets          = var.num_sockets
+#   power_state          = var.power_state
+#   project_id           = local.project_id
+#   subnet_id            = local.subnet_id
+#
+#   ubuntu_password  = var.ubuntu_password
+#   ubuntu_ssh_keys  = var.ubuntu_ssh_keys
+#   admin_users      = var.admin_users
+#   additional_users = [each.value]
+#
+#   vm_name = "${var.vm_name_prefix}-${each.key}"
+# }
---
+++ implementation/task_example/variables.tf
@@ -0,0 +1,63 @@
+# ---------------------------------------------------------------------------
+# Provider Variables
+# ---------------------------------------------------------------------------
+
+# variable "nutanix_endpoint" {
+#   description = "Nutanix Prism Central endpoint"
+#   type        = string
+# }
+
+# variable "nutanix_password" {
+#   description = "Nutanix password"
+#   sensitive   = true
+#   type        = string
+# }
+
+# variable "nutanix_port" {
+#   description = "Nutanix API port"
+#   type        = number
+#   default     = 9440
+# }
+
+# variable "nutanix_username" {
+#   description = "Nutanix username"
+#   type        = string
+# }
+
+# ---------------------------------------------------------------------------
+# Infrastructure Variables
+# ---------------------------------------------------------------------------
+
+# variable "cluster_uuid" {
+#   description = "UUID of the cluster used for project and VM placement"
+#   type        = string
+# }
+
+# variable "default_subnet_uuid" {
+#   description = "UUID of the subnet used for project and VM placement"
+#   type        = string
+# }
+
+# variable "project_name" {
+#   description = "Name of the Nutanix project to create and use for VM placement"
+#   type        = string
+# }
+
+# ---------------------------------------------------------------------------
+# VM Variables
+# ---------------------------------------------------------------------------
+
+# variable "disk_size_gb" {
+#   description = "Size of the SCSI boot disk in GB"
+#   type        = number
+# }
+
+# variable "image_uuid" {
+#   description = "UUID of the image to boot on VMs"
+#   type        = string
+# }
+
+# variable "vm_name_prefix" {
+#   description = "Prefix for VM names"
+#   type        = string
+# }
